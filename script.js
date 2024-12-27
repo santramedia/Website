004fa76e01ae3801d4ce75013b4483f4c9dc15d0
Santramedia <santramediaa@gmail.com>
2024-12-27 11:20:51 +0700

    change parameter + script js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function toggleMenu() {
-    const menu = document.getElementById('dropdownMenu');
-    menu.classList.toggle('active');
-}
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
